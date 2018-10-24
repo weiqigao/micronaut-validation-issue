f757286d31a5ba9eec295b3da6020a11e82f6726
Weiqi Gao <weiqigao@gmail.com>
2018-10-24 10:35:39 -0500

    Ported to Java 8

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module validation.issue {
-    exports foo;
-    opens foo to io.micronaut.inject;
-
-    requires io.micronaut.inject;
-    requires io.micronaut.validation;
-    requires java.validation;
-    requires javax.inject;
-}
